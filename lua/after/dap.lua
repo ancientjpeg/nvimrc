3f8ae5a984bf8a87a4f5fa5520551efda7b73d60
Jackson Kaplan <JwyattK@gmail.com>
2024-03-06 13:28:25 -0800

    new dap keybinds

--- lua/after/dap.lua
+++ lua/after/dap.lua
@@ -58,16 +58,23 @@ vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl =
 
 -- keymappings
 
-vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
-vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
-vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
-vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
-vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
-vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
-vim.keymap.set('n', '<Leader>lp',
+
+-- tenative
+vim.keymap.set('n', '<leader>dd', function() require('dap').continue() end)
+vim.keymap.set('n', '<leader>dn', function() require('dap').step_over() end)
+vim.keymap.set('n', '<leader>di', function() require('dap').step_into() end)
+vim.keymap.set('n', '<leader>do', function() require('dap').step_out() end)
+
+-- vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+-- vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+-- vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+-- vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>dB', function() require('dap').set_breakpoint() end)
+vim.keymap.set('n', '<Leader>dl',
   function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
-vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+vim.keymap.set('n', '<Leader>dc', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<Leader>dr', function() require('dap').run_last() end)
 vim.keymap.set('n', '<Leader>dq', function() require('dap').terminate() end)
 vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
   require('dap.ui.widgets').hover()
