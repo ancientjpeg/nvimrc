7ab6051f8ac959bbc512114841cd0fcb27cf0584
Jackson Kaplan <JwyattK@gmail.com>
2024-03-07 12:13:23 -0800

    update nvim-tree bind

--- lua/plugin/nvim-tree.lua
+++ lua/plugin/nvim-tree.lua
@@ -7,7 +7,7 @@ return {
     -- optionally enable 24-bit colour
     vim.opt.termguicolors = true
 
-
+    vim.keymap.set('n', '<leader>nt', vim.cmd.NvimTreeToggle);
     -- empty setup using defaults
     require("nvim-tree").setup()
   end
