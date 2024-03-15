9ffba7293269ee2bddc2d6b5a7b533b4766f46fa
Jackson Kaplan <JwyattK@gmail.com>
2024-03-15 11:26:22 -0700

    add rough nvimenv functionality

--- init.lua
+++ init.lua
@@ -8,3 +8,36 @@ vim.g.loaded_netrwPlugin = 1
 require("usr")
 require("plugin")
 require("after")
+
+-- run additional config if present
+local function local_config()
+  local cwd = vim.fn.getcwd()
+  local nvim_env_paths = vim.fs.find(
+    'nvimenv',
+    {
+      type = 'directory',
+      path = cwd,
+      limit = 1
+    }
+  )
+
+  if #nvim_env_paths ~= 1 then
+    return
+  end
+
+  local init_path = vim.fs.find('init.lua', {
+    path = nvim_env_paths[1],
+    limit = 1
+  })
+
+  if #init_path ~= 1 then
+    vim.print('Directory has .nvim.env but not init.lua!')
+    return
+  end
+
+
+  package.path = package.path .. vim.fn.getcwd()
+  require('nvimenv.init')
+end
+
+local_config()
