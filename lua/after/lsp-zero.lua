0b93e5eb580b30f35273c997a814241d131992d0
Jackson Kaplan <JwyattK@gmail.com>
2024-03-21 21:27:43 -0700

    improve lua lsp completions + add format prefs

--- lua/after/lsp-zero.lua
+++ lua/after/lsp-zero.lua
@@ -18,14 +18,14 @@ lsp_zero.set_sign_icons({
 
 lsp_zero.format_on_save({
   format_opts = {
-    async = false,
+    async      = false,
     timeout_ms = 10000,
   },
-  servers = {
-    ['clangd'] = { 'cpp' },
+  servers     = {
+    ['clangd']        = { 'cpp' },
     ['rust_analyzer'] = { 'rust' },
-    ['tsserver'] = { 'javascript', 'typescript' },
-    ['lua_ls'] = { 'lua' },
+    ['tsserver']      = { 'javascript', 'typescript' },
+    ['lua_ls']        = { 'lua' },
   }
 })
 
@@ -42,7 +42,6 @@ lspconfig.clangd.setup({
     vim.fn.has('win32') ~= 0 and 'clangd' or '/opt/homebrew/opt/llvm/bin/clangd', '--function-arg-placeholders=0'
   },
 })
-
 -- clangd-specific binding
 vim.keymap.set('n', 'ø', vim.cmd.ClangdSwitchSourceHeader)
 vim.keymap.set('n', '<M-o>', vim.cmd.ClangdSwitchSourceHeader)
@@ -63,25 +62,54 @@ require('mason-lspconfig').setup({
       require('lspconfig').eslint.setup({
         on_attach = function(client, bufnr)
           vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
+            buffer  = bufnr,
             command = "EslintFixAll",
           })
         end,
       })
     end,
     lua_ls = function()
-      -- silence vim warning in lua_ls
-      require('lspconfig').lua_ls.setup({
+      require('lspconfig').lua_ls.setup {
+        on_init  = function(client)
+          local path = client.workspace_folders[1].name
+          if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+            return
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime   = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT'
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library         = {
+                vim.env.VIMRUNTIME
+                -- Depending on the usage, you might want to add additional paths here.
+                -- "${3rd}/luv/library"
+                -- "${3rd}/busted/library",
+              }
+              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+              -- library = vim.api.nvim_get_runtime_file("", true)
+            }
+          })
+        end,
         settings = {
           Lua = {
             diagnostics = {
+              -- silence vim warning in lua_ls
               globals = { 'vim' }
-            }
-          }
-        }
+            },
+            format = {
+              enable                            = true,
+              align_continuous_assign_statement = "true",
+              align_continuous_rect_table_field = "true"
+            }, }
+        },
       }
-      )
-    end
+    end,
   }
 })
 
@@ -100,19 +128,19 @@ cmp.setup({
     { name = 'nvim_lsp' },
   },
   mapping = {
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-    ['<C-y>'] = cmp.mapping.confirm({ select = false }),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+    ['<CR>']   = cmp.mapping.confirm({ select = false }),
+    ['<C-y>']  = cmp.mapping.confirm({ select = false }),
+    ['<C-e>']  = cmp.mapping.abort(),
+    ['<Up>']   = cmp.mapping.select_prev_item({ behavior = 'select' }),
     ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
-    ['<C-p>'] = cmp.mapping(function()
+    ['<C-p>']  = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item({ behavior = 'insert' })
       else
         cmp.complete()
       end
     end),
-    ['<C-n>'] = cmp.mapping(function()
+    ['<C-n>']  = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_next_item({ behavior = 'insert' })
       else
