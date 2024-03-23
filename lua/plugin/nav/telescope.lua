d4be5e22d14fa14549cf7cd3dbeb40ecd41d2b84
Jackson Kaplan <JwyattK@gmail.com>
2024-03-23 16:15:46 -0700

    format telescope fzf build command

--- lua/plugin/nav/telescope.lua
+++ lua/plugin/nav/telescope.lua
@@ -8,8 +8,10 @@ return {
     -- fuzzy finder algo
     {
       'nvim-telescope/telescope-fzf-native.nvim',
-      build =
-      'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+      build = '\
+      cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && \
+      cmake --build build --config Release && \
+      cmake --install build --prefix build',
     },
     -- ripgrep args pass
     {
