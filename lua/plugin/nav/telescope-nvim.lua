37334e1c71bd86910319d9146a9d06c484aa49d6
Jackson Kaplan <JwyattK@gmail.com>
2024-03-24 13:29:35 -0700

    add ignore search

--- lua/plugin/nav/telescope-nvim.lua
+++ lua/plugin/nav/telescope-nvim.lua
@@ -41,6 +41,7 @@ return {
 
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fF', builtin.find_files, { no_ignore = true })
     vim.keymap.set('n', '<leader>fg', telescope.extensions.live_grep_args.live_grep_args)
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
