a80ed9a2b520b193648cd978631eb4675bc62dda
Jackson Kaplan <JwyattK@gmail.com>
2024-03-25 11:18:08 -0700

    all dap ui listeners unbounds

--- lua/plugin/debug/nvim-dap-ui.lua
+++ lua/plugin/debug/nvim-dap-ui.lua
@@ -6,17 +6,20 @@ return {
 
     dapui.setup()
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+    -- uncomment these if you want the UI to open/close automatically
+    -- dap.listeners.before.attach.dapui_config = function()
+    --   dapui.open()
+    -- end
+    -- dap.listeners.before.launch.dapui_config = function()
+    --   dapui.open()
+    -- end
+    -- dap.listeners.before.event_terminated.dapui_config = function()
+    --   dapui.close()
+    -- end
+    -- dap.listeners.before.event_exited.dapui_config = function()
+    --   dapui.close()
+    -- end
+
+    vim.keymap.set('n', '<leader>du', dapui.toggle)
   end
 }
