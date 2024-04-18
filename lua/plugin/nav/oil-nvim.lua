38be4c8aa8908853fe71e2c4fffe4653faab6328
Jackson Kaplan <JwyattK@gmail.com>
2024-04-18 16:13:04 -0700

    fixup oil

--- lua/plugin/nav/oil-nvim.lua
+++ lua/plugin/nav/oil-nvim.lua
@@ -1,15 +1,15 @@
 return {
   'stevearc/oil.nvim',
+  lazy         = false,
   opts         = {
     keymaps = {
       ["<leader>H"] = "actions.parent",
       ["<leader>L"] = "actions.select",
     }
   },
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys         = {
+  dependencies  = { "nvim-tree/nvim-web-devicons" },
+  keys          = {
     { "-", "<cmd>Oil <cr>",                   desc = "Oil - Open Parent Dir" },
     { "_", "<cmd>execute 'Oil' getcwd()<cr>", desc = "Oil - Open Current Working Dir" },
   }
-
 }
