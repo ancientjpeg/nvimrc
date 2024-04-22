91ed9538cf842a6107ffdedc6a060f7a0b05faa9
Jackson Kaplan <JwyattK@gmail.com>
2024-04-22 13:57:04 -0700

    add (commented) lazygit term

--- lua/plugin/misc/toggleterm-nvim.lua
+++ lua/plugin/misc/toggleterm-nvim.lua
@@ -6,6 +6,18 @@ return {
       open_mapping    = [[<C-s>]],
       direction       = 'float',
       insert_mappings = true,
-    }
+    },
+    -- config = function(opts)
+    --   require('toggleterm').setup(opts)
+
+    --   local Terminal = require('toggleterm.terminal').Terminal
+    --   local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+
+    --   function _LAZYGIT_TOGGLE()
+    --     lazygit:toggle()
+    --   end
+
+    --   vim.keymap.set("n", "<leader>g", _LAZYGIT_TOGGLE, { noremap = true, silent = true })
+    -- end
   }
 }
