d24544f3ea889160b6407e86a63b317ebcebe524
Jackson Kaplan <JwyattK@gmail.com>
2024-04-22 12:56:01 -0700

    edit noice settings

--- lua/plugin/editor/noice-nvim.lua
+++ lua/plugin/editor/noice-nvim.lua
@@ -12,11 +12,42 @@ return {
     },
     -- you can enable a preset for easier configuration
     presets = {
-      bottom_search         = true,  -- use a classic bottom cmdline for search
-      command_palette       = true,  -- position the cmdline and popupmenu together
-      long_message_to_split = true,  -- long messages will be sent to a split
+      bottom_search         = false, -- use a classic bottom cmdline for search
+      command_palette       = false, -- position the cmdline and popupmenu together
+      long_message_to_split = false, -- long messages will be sent to a split
       inc_rename            = false, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border        = false, -- add a border to hover docs and signature help
+    },
+
+    views   = {
+      cmdline_popup = {
+        position = {
+          row = 10,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 13,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 15,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      },
     },
   },
   dependencies = {
