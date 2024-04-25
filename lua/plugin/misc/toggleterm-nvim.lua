914d73f69bb4b725b2a2e14481d4ec8d40f1e993
Jackson Kaplan <JwyattK@gmail.com>
2024-04-25 11:40:57 -0700

    more toggleterm stuff

--- lua/plugin/misc/toggleterm-nvim.lua
+++ lua/plugin/misc/toggleterm-nvim.lua
@@ -2,13 +2,8 @@ return {
   {
     'akinsho/toggleterm.nvim',
     version = "*",
-    opts = {
-      open_mapping    = [[<C-s>]],
-      direction       = 'float',
-      insert_mappings = true,
-    },
-    config = function(opts)
-      require('toggleterm').setup(opts)
+    config = function(plugin, opts)
+      require("toggleterm").setup(opts)
 
       local Terminal = require('toggleterm.terminal').Terminal
       local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
@@ -18,6 +13,11 @@ return {
       end
 
       vim.keymap.set("n", "<leader>g", _LAZYGIT_TOGGLE, { noremap = true, silent = true })
-    end
+    end,
+    opts = {
+      open_mapping    = [[<C-s>]],
+      direction       = 'float',
+      insert_mappings = false,
+    },
   }
 }
