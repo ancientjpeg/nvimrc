cd8c2e4f1e830c2ffb3b110b1776a2624bbe2e79
Jackson Kaplan <JwyattK@gmail.com>
2024-05-04 16:31:37 -0700

    move nvim-cmp to its own file

--- lua/plugin/lsp/lsp-zero.lua
+++ lua/plugin/lsp/lsp-zero.lua
@@ -123,7 +123,8 @@ return {
                   enable                            = true,
                   align_continuous_assign_statement = "true",
                   align_continuous_rect_table_field = "true"
-                }, }
+                },
+              },
             },
           }
         end,
@@ -136,45 +137,6 @@ return {
       pattern = "*.py",
       command = "silent !black %",
       group = group,
-    })
-
-    local cmp = require('cmp')
-
-    cmp.setup({
-      sources = {
-        -- TODO i don't know what this does, please remember and then document it
-        { name = 'nvim_lsp' },
-      },
-      mapping = {
-        ['<CR>']   = cmp.mapping.confirm({ select = false }),
-        ['<C-y>']  = cmp.mapping.confirm({ select = false }),
-        ['<C-e>']  = cmp.mapping.abort(),
-        ['<Up>']   = cmp.mapping.select_prev_item({ behavior = 'select' }),
-        ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
-        ['<C-p>']  = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_prev_item({ behavior = 'insert' })
-          else
-            cmp.complete()
-          end
-        end),
-        ['<C-n>']  = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_next_item({ behavior = 'insert' })
-          else
-            cmp.complete()
-          end
-        end),
-      },
-      snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end,
-      },
-      preselect = 'item',
-      completion = {
-        completeopt = 'menu,menuone,noinsert'
-      },
     })
   end
 }
