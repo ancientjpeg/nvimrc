b1cc386675f168bf5f25459a807ab7c432b2f67c
Jackson Kaplan <JwyattK@gmail.com>
2024-05-04 17:07:23 -0700

    refactor lazy to use module loader (organize later)

--- init.lua
+++ init.lua
@@ -16,7 +16,34 @@ CONFIG_LOG('===== BEGIN CONFIG LOG =====')
 
 -- source all usr config first
 require("core")
-require("plugin")
+
+-- install lazy.nvim
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = {
+  install = {
+    colorscheme = {
+      'everforest'
+    }
+  }
+}
+
+-- setup lazy.nvim and source plugin specs
+require('lazy').setup(
+  "plugin",
+  lazy_config
+)
 
 -- run additional config if present
 local function local_config()
