2850c4e55d049994fbaab98ee546fd610e063071
Jackson Kaplan <JwyattK@gmail.com>
2024-05-07 18:48:02 -0700

    mostly revert toggleterm

--- lua/plugin/toggleterm-nvim.lua
+++ lua/plugin/toggleterm-nvim.lua
@@ -2,6 +2,11 @@ return {
   {
     'akinsho/toggleterm.nvim',
     version = "*",
+    opts = {
+      open_mapping    = [[<C-t>]],
+      direction       = 'float',
+      insert_mappings = false,
+    },
     config = function(plugin, opts)
       require("toggleterm").setup(opts)
 
@@ -18,26 +23,6 @@ return {
       vim.api.nvim_create_user_command("Yarn", function()
         yarndev:toggle()
       end, {})
-
-      function _G.set_terminal_keymaps()
-        local map_opts = { buffer = 0 }
-        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], map_opts)
-        -- vim.keymap.set('t', 'jk', [[<C-\><C-n>]], map_opts)
-        -- vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], map_opts)
-        -- vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], map_opts)
-        -- vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], map_opts)
-        -- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], map_opts)
-        -- vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], map_opts)
-      end
-
-      -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-      vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
     end,
-    opts = {
-      open_mapping    = [[<C-\>]],
-      size            = 22,
-      direction       = 'horizontal',
-      insert_mappings = false,
-    },
   }
 }
