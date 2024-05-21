e5b73168c905ac1cb71c109145f0136e81cabfc0
Jackson Kaplan <JwyattK@gmail.com>
2024-05-21 14:07:41 -0700

    no auto-close for md preview

--- lua/plugin/markdown-preview.lua
+++ lua/plugin/markdown-preview.lua
@@ -1,9 +1,11 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
+return
+{
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop', },
+  build = 'cd app && yarn install',
   init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_filetypes = { 'markdown', }
+    vim.g.mkdp_auto_close = 0
   end,
-  ft = { "markdown" },
+  ft = { 'markdown', },
 }
