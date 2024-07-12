9c9b871e33c4f79eb1f5ecfa22e670e4a9153996
Jackson Kaplan <JwyattK@gmail.com>
2024-07-12 13:05:02 -0700

    add cmake

---
+++ lua/plugin/cmake.lua
@@ -0,0 +1,25 @@
+return
+{
+  'Civitasv/cmake-tools.nvim',
+  config = true,
+  opts =
+  {
+    cmake_build_directory = 'build',
+    cmake_soft_link_compile_commands = true,
+    -- cmake_executor =
+    -- {
+    --   name = 'toggleterm',
+    --   opts =
+    --   {
+    --     direction = 'float', -- 'vertical' | 'horizontal' | 'tab' | 'float'
+    --     close_on_exit = false, -- whether close the terminal when exit
+    --     auto_scroll = true,  -- whether auto scroll to the bottom
+    --   },
+    -- },
+  },
+  keys =
+  {
+    { '<leader>cg', '<cmd>CMakeGenerate<cr>', },
+    { '<leader>cb', '<cmd>CMakeBuild<cr>', },
+  },
+}
