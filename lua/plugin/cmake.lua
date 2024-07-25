679d4a50e10cdc24450b2ecc001ae5092e3abbb9
Jackson Kaplan <JwyattK@gmail.com>
2024-07-25 10:52:08 -0700

    add cmake debug ability and new key commands

--- lua/plugin/cmake.lua
+++ lua/plugin/cmake.lua
@@ -6,6 +6,13 @@ return
   {
     cmake_build_directory = 'build',
     cmake_soft_link_compile_commands = true,
+    cmake_dap_configuration =
+    { -- debug settings for cmake
+      name = 'CMake - cpp',
+      type = 'lldb',
+      request = 'launch',
+      stopOnEntry = false,
+    },
     -- cmake_executor =
     -- {
     --   name = 'toggleterm',
@@ -21,6 +28,9 @@ return
   {
     { '<leader>cg', '<cmd>CMakeGenerate<cr>', },
     { '<leader>cb', '<cmd>CMakeBuild<cr>', },
+    { '<leader>cd', '<cmd>CMakeDebug<cr>', },
+    { '<leader>cr', '<cmd>CMakeRun<cr>', },
     { '<leader>cq', '<cmd>CMakeStopExecutor<cr>', },
+    { '<leader>cQ', '<cmd>CMakeStopRunner<cr>', },
   },
 }
