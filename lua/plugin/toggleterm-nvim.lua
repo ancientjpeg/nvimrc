d366de4e65863ef3a8a51128648c7f5c9dd0b7fd
Jackson Kaplan <JwyattK@gmail.com>
2024-07-29 12:43:51 -0700

    format toggleterm

--- lua/plugin/toggleterm-nvim.lua
+++ lua/plugin/toggleterm-nvim.lua
@@ -1,28 +1,30 @@
-return {
+return
+{
   {
     'akinsho/toggleterm.nvim',
-    version = "*",
-    opts = {
+    version = '*',
+    opts =
+    {
       open_mapping    = [[<C-t>]],
       direction       = 'float',
       insert_mappings = false,
     },
     config = function(plugin, opts)
-      require("toggleterm").setup(opts)
+      require('toggleterm').setup(opts)
 
       local Terminal = require('toggleterm.terminal').Terminal
-      local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
-      local yarndev  = Terminal:new({ cmd = "yarn dev", hidden = true, direction = "float", close_on_exit = false })
+      local lazygit  = Terminal:new({ cmd = 'lazygit', hidden = true, direction = 'float', })
+      local yarndev  = Terminal:new({ cmd = 'yarn dev', hidden = true, direction = 'float', close_on_exit = false, })
 
       function _LAZYGIT_TOGGLE()
         lazygit:toggle()
       end
 
-      vim.keymap.set("n", "<leader>g", _LAZYGIT_TOGGLE, { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>g', _LAZYGIT_TOGGLE, { noremap = true, silent = true, })
 
-      vim.api.nvim_create_user_command("Yarn", function()
+      vim.api.nvim_create_user_command('Yarn', function()
         yarndev:toggle()
       end, {})
     end,
-  }
+  },
 }
