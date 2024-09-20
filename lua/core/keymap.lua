d112b338f9898bd72961f3ca596f1df0a5b2e94a
Jackson Kaplan <JwyattK@gmail.com>
2024-09-19 18:32:00 -0700

    add new quickfix keybinds

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -5,25 +5,27 @@ vim.keymap.set('i', 'jk', '<Esc>')
 
 -- primeagen keybinds
 -- keep cursor in center when moving rapidly
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set('x', '<leader>p', [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ 'n', 'v', }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set({ 'n', 'v', }, '<leader>d', [["_d]])
 
 -- pane split keymaps
-vim.keymap.set("n", "<leader>s", function() vim.cmd('split') end)
-vim.keymap.set("n", "<leader>v", function() vim.cmd('vsplit') end)
+vim.keymap.set('n', '<leader>s', function() vim.cmd('split') end)
+vim.keymap.set('n', '<leader>v', function() vim.cmd('vsplit') end)
 
 -- quickfix quickbinds
-vim.keymap.set("n", "<leader>qq", "<cmd>cclose<CR>");
-vim.keymap.set("n", "<leader>qo", "<cmd>copen<CR>");
+vim.keymap.set('n', '<leader>qq', '<cmd>cclose<CR>');
+vim.keymap.set('n', '<leader>qo', '<cmd>copen<CR>');
+vim.keymap.set('n', '<leader>qp', '<cmd>cp<CR>');
+vim.keymap.set('n', '<leader>qn', '<cmd>cn<CR>');
