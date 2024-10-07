f50b61e12416155841a4b70dfd936f0a2dcdca96
Jackson Kaplan <JwyattK@gmail.com>
2024-10-07 14:13:47 -0700

    os-specific clangd call...

--- lua/plugin/lsp-zero.lua
+++ lua/plugin/lsp-zero.lua
@@ -55,7 +55,7 @@ return
         init_options = { compilationDatabasePath = 'build', },
         cmd =
         {
-          'clangd',
+          vim.fn.has('macunix') and '/opt/homebrew/opt/llvm/bin/clangd' or 'clangd',
           '--function-arg-placeholders=0',
           '--clang-tidy',
           '--background-index',
