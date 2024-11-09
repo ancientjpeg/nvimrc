25f4f5fb3b50df59d5d31c488a35f8da86dd6136
Jackson Kaplan <JwyattK@gmail.com>
2024-11-09 12:02:02 +0000

    neogen chance default copyright statement

--- lua/plugin/neogen.lua
+++ lua/plugin/neogen.lua
@@ -1,40 +1,54 @@
-return {
-  "danymat/neogen",
+return
+{
+  'danymat/neogen',
   config = function()
-    local i             = require("neogen.types.template").item
-    local doxygen_setup = {
-      { nil,         "/**",                                               { no_results = true, type = { "func", "file", "class" } } },
-      { nil,         " * @file $1",                                       { no_results = true, type = { "file" } } },
-      { nil,         " * @author Jackson Kaplan (jackson@minimal.audio)", { no_results = true, type = { "file" } } },
-      { nil,         " * @date " .. os.date("%Y-%m-%d"),                  { no_results = true, type = { "file" } } },
-      { nil,         " * Copyright © 2024 Minimal. All rights reserved.", { no_results = true, type = { "file" } } },
-      { nil,         " */",                                               { no_results = true, type = { "func", "file", "class" } } },
-      { nil,         "",                                                  { no_results = true, type = { "file" } } },
+    local i             = require('neogen.types.template').item
+    local doxygen_setup =
+    {
+      { nil, '/**',                                               { no_results = true, type = { 'func', 'file', 'class', }, }, },
+      { nil, ' * @file $1',                                       { no_results = true, type = { 'file', }, }, },
+      { nil, ' * @author Jackson Kaplan (jackson@minimal.audio)', { no_results = true, type = { 'file', }, }, },
+      { nil, ' * @date ' .. os.date('%Y-%m-%d'),                  { no_results = true, type = { 'file', }, }, },
+      { nil, ' * Copyright (c) 2024. All rights reserved.',       { no_results = true, type = { 'file', }, }, },
+      {
+        nil,
+        ' */',
+        {
+          no_results = true,
+          type = { 'func', 'file', 'class', },
+        },
+      },
+      { nil,         '',                 { no_results = true, type = { 'file', }, }, },
 
-      { nil,         "/**",                                               { type = { "func", "class", "type" } } },
-      { i.ClassName, " * @class %s",                                      { type = { "class" } } },
-      { i.Type,      " * @typedef %s",                                    { type = { "type" } } },
-      { nil,         " * @brief $1",                                      { type = { "func", "class", "type" } } },
-      { nil,         " *",                                                { type = { "func", "class", "type" } } },
-      { i.Tparam,    " * @tparam %s $1" },
-      { i.Parameter, " * @param %s $1" },
-      { i.Return,    " * @return $1" },
-      { nil,         " */",                                               { type = { "func", "class", "type" } } },
+      { nil,         '/**',              { type = { 'func', 'class', 'type', }, }, },
+      { i.ClassName, ' * @class %s',     { type = { 'class', }, }, },
+      { i.Type,      ' * @typedef %s',   { type = { 'type', }, }, },
+      { nil,         ' * @brief $1',     { type = { 'func', 'class', 'type', }, }, },
+      { nil,         ' *',               { type = { 'func', 'class', 'type', }, }, },
+      { i.Tparam,    ' * @tparam %s $1', },
+      { i.Parameter, ' * @param %s $1', },
+      { i.Return,    ' * @return $1', },
+      { nil,         ' */',              { type = { 'func', 'class', 'type', }, }, },
     }
 
-    require('neogen').setup({
-      languages = {
-        cpp = {
-          template = {
-            annotation_convention = "custom_doxygen",
-            custom_doxygen = doxygen_setup
-          }
-        }
-      }
-    })
+    require('neogen').setup(
+      {
+        languages =
+        {
+          cpp =
+          {
+            template =
+            {
+              annotation_convention = 'custom_doxygen',
+              custom_doxygen = doxygen_setup,
+            },
+          },
+        },
+      })
   end,
-  keys   = {
-    { "<leader>nf", "<cmd>Neogen<cr>",      desc = "Neogen Generate" },
-    { "<leader>nF", "<cmd>Neogen file<cr>", desc = "Neogen Generate File Docstring" },
+  keys   =
+  {
+    { '<leader>nf', '<cmd>Neogen<cr>',      desc = 'Neogen Generate', },
+    { '<leader>nF', '<cmd>Neogen file<cr>', desc = 'Neogen Generate File Docstring', },
   },
 }
