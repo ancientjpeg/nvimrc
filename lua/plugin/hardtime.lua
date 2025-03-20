c8d131904721acd1ca75a1157e3fca2e8da6157c
Jackson Kaplan <JwyattK@gmail.com>
2025-03-20 16:24:09 -0700

    remove hardtime

--- lua/plugin/hardtime.lua
+++ lua/plugin/hardtime.lua
@@ -1,6 +1,6 @@
-return
-{
-  'm4xshen/hardtime.nvim',
-  dependencies = { 'MunifTanjim/nui.nvim', },
-  opts = {},
-}
+return {}
+-- {
+--   'm4xshen/hardtime.nvim',
+--   dependencies = { 'MunifTanjim/nui.nvim', },
+--   opts = {},
+-- }
