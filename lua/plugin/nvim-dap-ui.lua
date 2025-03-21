657b1f13808972d451728bc27887abe60578e2f0
Jackson Kaplan <JwyattK@gmail.com>
2025-03-20 17:24:31 -0700

    add layouts to dapui setup, make bigger by default

--- lua/plugin/nvim-dap-ui.lua
+++ lua/plugin/nvim-dap-ui.lua
@@ -5,7 +5,50 @@ return
   config       = function()
     local dap, dapui = require('dap'), require('dapui')
 
-    dapui.setup({})
+    dapui.setup(
+      {
+        layouts =
+        {
+          {
+            elements =
+            {
+              {
+                id = 'scopes',
+                size = 0.25,
+              },
+              {
+                id = 'breakpoints',
+                size = 0.25,
+              },
+              {
+                id = 'stacks',
+                size = 0.25,
+              },
+              {
+                id = 'watches',
+                size = 0.25,
+              },
+            },
+            position = 'left',
+            size = 60,
+          },
+          {
+            elements =
+            {
+              {
+                id = 'repl',
+                size = 0.5,
+              },
+              {
+                id = 'console',
+                size = 0.5,
+              },
+            },
+            position = 'bottom',
+            size = 30,
+          },
+        },
+      })
 
     -- uncomment these if you want the UI to open/close automatically
     -- dap.listeners.before.attach.dapui_config = function()
