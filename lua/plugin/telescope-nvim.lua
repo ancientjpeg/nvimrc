054c1852f7105e8d903c97c884e4665a35f58132
Jackson Kaplan <JwyattK@gmail.com>
2025-03-24 13:32:17 -0700

    add telescope select

--- lua/plugin/telescope-nvim.lua
+++ lua/plugin/telescope-nvim.lua
@@ -19,6 +19,7 @@ return
       -- For major updates, this must be adjusted manually.
       version = '^1.0.0',
     },
+    { 'nvim-telescope/telescope-ui-select.nvim', },
   },
   config = function()
     local telescope = require('telescope')
@@ -56,9 +57,11 @@ return
             override_file_sorter    = true,          -- override the file sorter
             case_mode               = 'ignore_case', -- or "ignore_case" or "respect_case"
           },
+
         },
       })
     telescope.load_extension('fzf')
+    telescope.load_extension('ui-select')
 
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
