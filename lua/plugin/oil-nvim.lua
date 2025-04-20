4c969104bb3b9ae22d3ffb55f8ab315fbe35aa37
Jackson Kaplan <JwyattK@gmail.com>
2025-04-20 16:09:15 -0700

    oil scp annoying fix

--- lua/plugin/oil-nvim.lua
+++ lua/plugin/oil-nvim.lua
@@ -1,15 +1,20 @@
-return {
+return
+{
   'stevearc/oil.nvim',
   lazy         = false,
-  opts         = {
-    keymaps = {
-      ["<leader>H"] = "actions.parent",
-      ["<leader>L"] = "actions.select",
-    }
+  opts         =
+  {
+    silence_scp_warning = true,
+    keymaps =
+    {
+      ['<leader>H'] = 'actions.parent',
+      ['<leader>L'] = 'actions.select',
+    },
   },
-  dependencies  = { "nvim-tree/nvim-web-devicons" },
-  keys          = {
-    { "-", "<cmd>Oil <cr>",                   desc = "Oil - Open Parent Dir" },
-    { "_", "<cmd>execute 'Oil' getcwd()<cr>", desc = "Oil - Open Current Working Dir" },
-  }
+  dependencies = { 'nvim-tree/nvim-web-devicons', },
+  keys         =
+  {
+    { '-', '<cmd>Oil <cr>',                   desc = 'Oil - Open Parent Dir', },
+    { '_', "<cmd>execute 'Oil' getcwd()<cr>", desc = 'Oil - Open Current Working Dir', },
+  },
 }
