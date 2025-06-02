28bbddc8c41b69fe6f5ff27ca5112f343aebff0b
Jackson Kaplan <JwyattK@gmail.com>
2025-06-01 22:02:10 -0700

    add necessary schemes for color script

--- lua/plugin/colorscheme.lua
+++ lua/plugin/colorscheme.lua
@@ -1,26 +1,10 @@
 return
 {
   'dasupradyumna/midnight.nvim',
+  dependencies = { 'catppuccin/nvim', },
   lazy = false,
   priority = 1000,
   config = function()
     vim.cmd.colorscheme 'midnight'
   end,
 }
--- {
---   'neanias/everforest-nvim',
---   version = false,
---   lazy = false,
---   priority = 1000, -- make sure to load this before all the other start plugins
---   -- Optional; default configuration will be used if setup isn't called.
---   config = function()
---     require('everforest').setup(
---       {
---         background = 'soft',
---         transparent_background_level = 1,
---         -- Your config here
---       })
-
---     vim.cmd('colorscheme everforest')
---   end,
--- }
