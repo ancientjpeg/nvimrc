7b3adec5c26d826c03bbfef741eb14db0604c0d6
Jackson Kaplan <JwyattK@gmail.com>
2025-09-28 16:01:24 -0700

    auto dark mode with light cyberdream

--- lua/plugin/colorscheme.lua
+++ lua/plugin/colorscheme.lua
@@ -1,10 +1,30 @@
 return
 {
-  'dasupradyumna/midnight.nvim',
-  dependencies = { 'catppuccin/nvim', },
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme 'midnight'
-  end,
+  {
+    'dasupradyumna/midnight.nvim',
+    dependencies = { 'catppuccin/nvim', },
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    'scottmckendry/cyberdream.nvim',
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    'f-person/auto-dark-mode.nvim',
+    opts =
+    {
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value('background', 'dark', {})
+        vim.cmd.colorscheme('midnight')
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value('background', 'light', {})
+        vim.cmd.colorscheme('cyberdream-light')
+      end,
+      update_interval = 3000,
+      fallback = 'dark',
+    },
+  },
 }
